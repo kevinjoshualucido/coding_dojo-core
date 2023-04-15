379ed7236c914227ecfcf863dde1295ee219b6cc
Kevin Lucido <112150217+kevinjoshualucido@users.noreply.github.com>
2023-04-15 00:35:11 -0500

    Add files via upload

--- bank_account.py
+++ bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    all_transc = []
+    all_instances = []
 
     def __init__(self, int_rate, bal):
         self.int_rate = int_rate
@@ -7,12 +7,16 @@ class BankAccount:
 
     def deposit(self, amt):
         self.bal += amt
-        print('Deposit:', self.bal)
+        print('Deposit:', round(self.bal, 2))
         return self
 
     def withdraw(self, amt):
-        self.bal -= amt
-        print('Withdraw:', self.bal)
+        if self.bal < amt:
+            print('Insufficient funds: Charging a 5.00 fee')
+            self.bal -= 5
+        else:
+            self.bal -= amt
+        print('Withdraw:', round(self.bal, 2))
         return self
 
     def disp_acc_info(self):
@@ -25,9 +29,7 @@ class BankAccount:
         print('Yielded interest:', round(yielded_interest, 2))
         return self
     
-    @classmethod
-    def get_transc(cls):
-        print(cls.all_transc)
+    
 
 
 account1 = BankAccount(0.0431, 150.00)
@@ -35,7 +37,4 @@ account2 = BankAccount(0.0392, 380.00)
 
 
 account1.deposit(150.00).deposit(1_000.00).deposit(1_347.00).withdraw(2_513.00).int_yld().disp_acc_info()
-account2.deposit(1001.17).deposit(1343.57).withdraw(25.04).withdraw(9.99).withdraw(571.73).withdraw(550.00).int_yld().disp_acc_info()
-
-account1.get_transc()
-account2.get_transc()
+account2.deposit(1001.17).deposit(1343.57).withdraw(25.04).withdraw(9999.99).withdraw(571.73).withdraw(550.00).int_yld().disp_acc_info()  # should initiate a $5 insufficient funds fee
